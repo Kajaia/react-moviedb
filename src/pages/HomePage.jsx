6f4ecc8bfeef3b8cbc91e62068222cdc153ec7e9
Kajaia <lashakajaia31@gmail.com>
2022-10-18 22:08:56 +0400

    Added ErrorBoundary component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,32 +1,41 @@
+import ErrorBoundary from "../components/ErrorBoundary";
 import MovieList from "../components/movies/MovieList";
 import Search from "../components/others/Search";
 
 function HomePage() {
   return (
     <>
-      <Search />
+      <ErrorBoundary>
+        <Search />
+      </ErrorBoundary>
       <div className="container">
-        <MovieList
-          title="What's Popular"
-          endpoint="popular"
-          styles="d-flex movies-wrapper"
-        />
+        <ErrorBoundary>
+          <MovieList
+            title="What's Popular"
+            endpoint="popular"
+            styles="d-flex movies-wrapper"
+          />
+        </ErrorBoundary>
       </div>
       <div className="bg-light-blue text-white">
         <div className="container">
-          <MovieList
-            title="Upcoming movies"
-            endpoint="upcoming"
-            styles="d-flex movies-wrapper"
-          />
+          <ErrorBoundary>
+            <MovieList
+              title="Upcoming movies"
+              endpoint="upcoming"
+              styles="d-flex movies-wrapper"
+            />
+          </ErrorBoundary>
         </div>
       </div>
       <div className="container">
-        <MovieList
-          title="Top Rated"
-          endpoint="top_rated"
-          styles="d-flex movies-wrapper"
-        />
+        <ErrorBoundary>
+          <MovieList
+            title="Top Rated"
+            endpoint="top_rated"
+            styles="d-flex movies-wrapper"
+          />
+        </ErrorBoundary>
       </div>
     </>
   );
--- src/pages/PopularMoviesPage.jsx
+++ src/pages/PopularMoviesPage.jsx
@@ -1,13 +1,16 @@
 import MovieList from "../components/movies/MovieList";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 function PopularMoviesPage() {
   return (
     <div className="container">
-      <MovieList
-        title="Popular Movies"
-        endpoint="popular"
-        styles="row justify-content-center"
-      />
+      <ErrorBoundary>
+        <MovieList
+          title="Popular Movies"
+          endpoint="popular"
+          styles="row justify-content-center"
+        />
+      </ErrorBoundary>
     </div>
   );
 }
